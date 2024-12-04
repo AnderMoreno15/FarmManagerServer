44f82920e3c18cc18b819d5b6f4a329ab15ffebb
aitziberE <eskizabel.aitziber@gmail.com>
2024-12-04 21:41:19 +0100

    create animal RESTful service structure

---
+++ src/java/services/AnimalFacade.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import entities.Animal;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Aitziber
+ */
+@Stateless
+public class AnimalFacade implements IAnimalFacade {
+
+    @PersistenceContext(unitName = "ServerSidePU")
+    private EntityManager em;
+
+    public List<Animal> findAll() {
+      return em.createQuery("select a from Animal a").getResultList();
+    }
+
+    public List<Animal> findByName(String name) {
+      return em.createQuery("select a from Animal a where UPPER(a.name) LIKE :animalName").
+                             setParameter("animalName", "%" + name.toUpperCase() + "%").getResultList();
+    }
+
+    @Override
+    public void businessMethod() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
