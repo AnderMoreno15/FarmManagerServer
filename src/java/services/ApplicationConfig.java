525660eb0f0fcd9871892c6c5a60f81a0ee0e732
Ander Moreno <amorenoch15@gmail.com>
2025-01-24 09:58:09 +0100

    application config modified

--- src/java/services/ApplicationConfig.java
+++ src/java/services/ApplicationConfig.java
@@ -33,9 +33,9 @@ public class ApplicationConfig extends Application {
         resources.add(services.AnimalGroupFacadeREST.class);
         resources.add(services.AnimalREST.class);
         resources.add(services.ConsumesFacadeREST.class);
+        resources.add(services.ManagerFacadeREST.class);
         resources.add(services.ProductEntityFacadeREST.class);
         resources.add(services.ProviderEntityFacadeREST.class);
-        resources.add(services.ManagerFacadeREST.class);
         resources.add(services.SpeciesREST.class);
         resources.add(services.UserEntityFacadeREST.class);
     }
