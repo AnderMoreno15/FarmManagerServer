f47ce94b52a2a335c9be6764089d241fed35424c
Ander Moreno <amorenoch15@gmail.com>
2025-02-06 19:49:14 +0100

    FacadeRESTs javadoc done. Users Ejbs fixed

--- src/java/services/user/UserEntityFacadeREST.java
+++ src/java/services/user/UserEntityFacadeREST.java
@@ -5,18 +5,8 @@
  */
 package services.user;
 
-import entities.UserEntity;
-import exceptions.CreateException;
-import exceptions.ReadException;
 import javax.ejb.EJB;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.InternalServerErrorException;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 /**
  *
